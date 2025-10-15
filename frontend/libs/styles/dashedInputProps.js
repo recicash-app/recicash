5fddbcce1ebf690284738e21b27c610544197369
Giovanna Uchôa <gio-uchoa@hotmail.com>
2025-10-15 00:13:52 -0300

    feat: enhance CardBox and LeafBox components, add decorative SVG assets and update layout in Test component
    Signed-off-by: Giovanna Uchôa <gio-uchoa@hotmail.com>

--- frontend/libs/styles/dashedInputProps.js
+++ frontend/libs/styles/dashedInputProps.js
@@ -4,9 +4,9 @@ export const getDashedInputProps = (isEditing) => ({
     padding: "8px",
     outline: "none",
     resize: "none",
-    border: isEditing ? "2px solid transparent" : "none",
+    border: isEditing ? "1px solid transparent" : "none",
     borderImage: isEditing
-      ? "repeating-linear-gradient(45deg, #B0B0B0 0 16px, transparent 16px 32px) 1"
+      ? "repeating-linear-gradient(45deg, #000000 0 16px, transparent 16px 32px) 1"
       : "none",
     borderImageRepeat: "round",
     background: isEditing ? "#D9D9D961" : "transparent",
