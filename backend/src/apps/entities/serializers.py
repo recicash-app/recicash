23195cbf6f3442370e4103b6fd8ccde6935a0784
rennisson <rennisson.alves@gmail.com>
2025-10-25 20:20:42 -0300

    Closes #69, closes #70, closes #71, closes #77: stablish admin authentication when requesting to posts endpoint

--- backend/src/apps/entities/serializers.py
+++ backend/src/apps/entities/serializers.py
@@ -1,26 +1,19 @@
 from rest_framework import serializers
-from .models import PostBlog
+from .models import PostBlog, PostImage
+        
+class PostImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostImage
+        fields = ('id', 'post_id', 'image')
+
 
 class PostBlogSerializer(serializers.ModelSerializer):
+    images = PostImageSerializer(many=True, read_only=True)
 
     class Meta:
         model = PostBlog
 
         # Post blog field that will be shown
-        fields = (
-            'post_id',
-            'author_id',
-            'title',
-            'text',
-            'images',
-            'created_at',
-            'last_edition_date'
-        )
-        
+        fields = ('post_id', 'author_id', 'title', 'text', 'images', 'created_at', 'last_edition_date')
         # Fields that cannot be edited by user
-        read_only_fields = (
-                        'post_id',
-                        'author_id',
-                        'created_at',
-                        'last_edition_date'
-                    )
+        read_only_fields = ('author_id', 'created_at', 'last_edition_date')
--- backend/src/apps/views/blog_view.py
+++ backend/src/apps/views/blog_view.py
@@ -1,8 +1,26 @@
 from rest_framework import viewsets
-from apps.entities.models import PostBlog
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from apps.entities.models import PostBlog, PostImage
 from apps.entities.serializers import PostBlogSerializer
+from apps.entities.permissions import IsAppAdminUser
 
-class AllPostBlogViewSet(viewsets.ModelViewSet):
+
+class PostBlogViewSet(viewsets.ModelViewSet):
     # Gets all posts from blog
     queryset = PostBlog.objects.all()
+    serializer_class = PostBlogSerializer
+
+    def get_permissions(self):
+        if self.action == 'list' or self.action == 'retrieve':
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAuthenticated, IsAppAdminUser]
+        
+        return [permission() for permission in permission_classes]
+
+    def perform_create(self, serializer):
+        serializer.save(author_id=self.request.user)
+
+class PostImageViewSet(viewsets.ModelViewSet):
+    queryset = PostImage.objects.all()
     serializer_class = PostBlogSerializer
