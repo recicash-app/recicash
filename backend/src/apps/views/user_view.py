7acb96b9ad4c4beebd0597b3a06082b4e92b8cc0
rennisson <rennisson.alves@gmail.com>
2025-11-02 21:20:04 -0300

    feat(entities): user sign up

--- backend/src/apps/views/user_view.py
+++ backend/src/apps/views/user_view.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from apps.entities.models import User
 from apps.entities.serializers import UserSerializer, UserObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -7,7 +7,19 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
+    
+    def get_permissions(self):
+        """
+        Define permissions for different roles
+        """
+        if self.action == 'create':
+            # Allows anyone to create a new user
+            permission_classes = [AllowAny]
+        else:
+            # Demands authentication for other roles than 'create'
+            permission_classes = [IsAuthenticated]
+            
+        return [permission() for permission in permission_classes]
 
 
 class UserObtainPairView(TokenObtainPairView):
