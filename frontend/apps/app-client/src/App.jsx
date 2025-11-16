814920b7651925e624a29d2720a7286e222dd70c
Júlia Calixto <juliacalixtorosaa@gmail.com>
2025-11-16 02:06:32 -0300

    feat: implements initial version of the landing page, with informational text about the system and a section on recyclable materials
    Signed-off-by: Júlia Calixto <juliacalixtorosaa@gmail.com>

--- frontend/apps/app-client/src/App.jsx
+++ frontend/apps/app-client/src/App.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
 
-import Header from './components/navigation/Header';
-import Home from './pages/Home';
 import Test from './Test';
+import Home from './pages/Home';
 import SignInPage from './pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
+import Header from './components/navigation/Header';
 
 function App() {
   return (
@@ -13,10 +13,10 @@ function App() {
       <Header/>
       <Box sx={{ mt: 4, pb: 4, px: 3, maxWidth: '100vw', overflowX: 'hidden' }}>
         <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/test" element={<Test />} />
-        <Route path="/cadastro" element={<SignUpPage />} />
-        <Route path="/login" element={<SignInPage />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/test" element={<Test />} />
+          <Route path="/cadastro" element={<SignUpPage />} />
+          <Route path="/login" element={<SignInPage />} />
         </Routes>
       </Box>
     </>
