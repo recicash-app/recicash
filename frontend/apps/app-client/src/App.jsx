a64e42cd2df0f885ec6ce237743fabfd2bf82126
Giovanna Uchôa <gio-uchoa@hotmail.com>
2025-11-16 00:07:24 -0300

    feat: add recycling instructions and images for paper, metal, glass, and plastic
    Signed-off-by: Giovanna Uchôa <gio-uchoa@hotmail.com>

--- frontend/apps/app-client/src/App.jsx
+++ frontend/apps/app-client/src/App.jsx
@@ -2,12 +2,18 @@ import { Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
 
 import Test from './Test';
+import { glassInstructions, paperInstructions, plasticInstructions, metalInstructions } from './data/recyclingInstructions';
+import PostLayout from '@shared/ui/PostLayout';
 
 function App() {
   return (
     <Box sx={{ mt: 4, pb: 4, px: 3, maxWidth: '100vw' }}>
       <Routes>
         <Route path="/test" element={<Test />} />
+        <Route path="/test-paper" element={<div style={{ padding: "16px "}}> <PostLayout data={paperInstructions} isEditing={false} /> </div>} />
+        <Route path="/test-plastic" element={<PostLayout data={plasticInstructions} isEditing={false} />} />
+        <Route path="/test-glass" element={<PostLayout data={glassInstructions} isEditing={false} />} />
+        <Route path="/test-metal" element={<PostLayout data={metalInstructions} isEditing={false} />} />
       </Routes>
     </Box>
   );
