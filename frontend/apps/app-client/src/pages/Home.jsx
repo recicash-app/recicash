9c60293b926e88a847b1d28c4f88ff052b6dbecb
Júlia Calixto <juliacalixtorosaa@gmail.com>
2025-11-23 19:39:46 -0300

    feat: update icon references and improve landing page layout; add new icons and adjust imports
    Signed-off-by: Júlia Calixto <juliacalixtorosaa@gmail.com>

--- frontend/apps/app-client/src/pages/Home.jsx
+++ frontend/apps/app-client/src/pages/Home.jsx
@@ -1,40 +1,49 @@
 import React from 'react';
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Box, Typography, Button, Container, Grid } from '@mui/material';
+
 import CardBox from '@shared/ui/CardBox';
-import LeafBox from '@shared/ui/LeafBox';
 import shapeTopLeft from '@shared/assets/shape-top-left.svg';
 import shapeTopRight from '@shared/assets/shape-top-right.svg';
 import shapePattern from '@shared/assets/pattern-plus-green-white.svg';
 
-import iconPlastic from '@shared/assets/icon-plastic.png';
-import iconPaper from '@shared/assets/icon-paper.png';
-import iconGlass from '@shared/assets/icon-glass.png';
-import iconMetal from '@shared/assets/icon-metal.png';
-import peoples from '@shared/assets/peoples.png';
+import iconPlastic from '../../public/icon-plastic.png';
+import iconPaper from '../../public/icon-paper.png';
+import iconGlass from '../../public/icon-glass.png';
+import iconMetal from '../../public/icon-metal.png';
 import FullScreenOverlay from "@shared/ui/FullScreenOverlay";
 
 
 function Home() {
+
   const [overlayOpen, setOverlayOpen] = useState(false);
+  const navigate = useNavigate();
 
   return (
     <Box sx={{ width: '100%', overflowX: 'hidden'}}>
+
       {/* Decorative shapes */}
-      <img src={shapeTopLeft} alt="" style={{ position: 'absolute', top: '0', left: '-332px', width: '39%', height: '525px', transform: 'rotate(0deg)', zIndex: '-1' }} />
+      <img 
+        src={shapeTopLeft} 
+        alt="" 
+        style={{ position: 'fixed', top: '0', left: '0', width: '40vw', zIndex: -1, pointerEvents: 'none', }} />
+        
       <img
         src={shapeTopRight}
         alt=""
-        style={{ position: 'absolute', top: '0', left:'680px', right: '0', width: '620px', height: '650px', transform: 'rotate(0.1deg)', opacity: '1', zIndex: '-1' }}
+        style={{ position: 'fixed', top: '0', right: '0', width: '50vw', zIndex: -1, pointerEvents: 'none', objectFit: 'scale-dow', objectPosition: 'top right' }}
       />
       
-      <Container maxWidth="lg" sx={{ padding: 0 }}>
+      <Box sx={{ position: 'relative', zIndex: 2 }}>
+        {/*<Container maxWidth="lg" sx={{ padding: 0, position: 'relative', zIndex: 1 }}>*/}
+        <Box sx={{ position: 'relative', zIndex: 1, width: '100%' }}>
         
-        {/* Hero Section */}
-        <Grid container spacing={2} alignItems="center" sx={{ mt: 3, flexWrap: 'nowrap' }}>
-          <Grid item xs={6} >
+        {/* Info Section   */}
+        <Grid container spacing={0} alignItems="center" sx={{ justifyContent: 'space-between', flexWrap: 'nowrap'}}>
+          <Grid item  sx={{ textAlign: 'left', width: '55%' }} >
             <Typography variant="h1" sx={{ mb: 5 }}>
-               ♻️ Recicash: descarte consciente que gera benefícios!
+              Recicash: descarte consciente que gera benefícios!
             </Typography>
             <Typography variant="body1" sx={{ mb: 2 }}>
               O Recicash é uma iniciativa que une sustentabilidade e benefícios reais para o cidadão. A ideia é simples: quanto mais você recicla, mais você ganha.
@@ -43,24 +52,33 @@ function Home() {
               Ao levar materiais recicláveis — como plástico, vidro, papel e metal — até um ecoponto parceiro, o participante acumula pontos no Recicash. Esses pontos podem ser trocados por cupons de desconto, benefícios locais ou até mesmo produtos sustentáveis.
             </Typography>
           </Grid>
-          <Grid item xs={6} sx={{mt: 0}}>
-            <img
-              src={peoples}
-              alt="Recicash People"
-              style={{ width: '100%', height: '100%', borderRadius: '8px' }}
-            />
+          <Grid item 
+            sx={{ 
+              width: '40%',
+              pl: 2,
+              display: 'flex', 
+              justifyContent: 'center', 
+              alignItems: 'center'     
+            }}>
+            <Box
+            component="img"
+            src="/icon-recycle-border.svg"
+            alt="recycle icon"
+            sx={{ width: "30vw", userSelect: "none", pointerEvent: "none", zIndex: 2 }}
+          />
           </Grid>
+          
         </Grid>
 
         <FullScreenOverlay open={overlayOpen} onClose={() => setOverlayOpen(false)} />
           
         {/* Materials Section */}
-        <Box sx={{ position: 'relative', mt: 5, pt: 5, pb: 5 }}>
-          <img src={shapePattern} alt="" style={{ position: 'absolute', top: '0', right: '0', width: '200px', zIndex: '-1' }} />
-          <Typography variant="h2" sx={{ mb: 3, textAlign: 'center', color: '#14183E' }}>
+        <Box sx={{ position: 'relative', mt: 5, pt: 5, pb: 5, justifyContent: 'center' }}>
+          <img src={shapePattern} alt="" style={{ position: 'absolute', top: '0', right: '0', width: '200px', zIndex: 0 }} />
+          <Typography variant="h2" sx={{ mb: 3, textAlign: 'center', color: '#14183E',zIndex: 1, position: 'relative' }}>
             Materiais Recicláveis
           </Typography>
-          <Grid container spacing={7} justifyContent="center">
+          <Grid container spacing={7} justifyContent="center" sx={{ boxShadow: 'none' }}>
             <CardBox onClick={() => setOverlayOpen(true)} sx={{ width: '200px', height: '250px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
               <img
                 src={iconPlastic}
@@ -104,11 +122,14 @@ function Home() {
           </Grid>
         </Box>
 
-        {/* Call to Action */}
+        {/* Sing Up Button */}
         <Box sx={{ textAlign: 'center', mt: 5 }}>
-          <Button variant="recicashCTA">Saiba mais</Button>
+          <Button variant="recicashCTA" onClick={() => navigate('/cadastro')} >Saiba mais</Button>
         </Box>
-      </Container>
+      </Box>
+
+      </Box>
+      
     </Box>
   );
 }
