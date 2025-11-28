4bf33fc60e984d653604aaf800455fbddda1d3b4
Giovanna Uchôa <gio-uchoa@hotmail.com>
2025-11-28 19:33:44 -0300

    refactor: authentication views and serializers, update URL routing for new login/logout endpoints
    Signed-off-by: Giovanna Uchôa <gio-uchoa@hotmail.com>

--- backend/src/core/infrastructure/serializers/__init__.py
+++ backend/src/core/infrastructure/serializers/__init__.py
@@ -1,5 +1,5 @@
-from .user_serializers import UserSerializer, UserObtainPairSerializer
+from .user_serializers import UserSerializer, LoginSerializer
 from .blog_serializers import PostImageSerializer, PostBlogSerializer
 from .recycling_serializers import RecyclingSerializer, EcopontoDisposalSerializer
 
-__all__ = ["UserSerializer", "UserObtainPairSerializer", "PostImageSerializer", "PostBlogSerializer", "RecyclingSerializer", "EcopontoDisposalSerializer"]
+__all__ = ["UserSerializer", "LoginSerializer", "PostImageSerializer", "PostBlogSerializer", "RecyclingSerializer", "EcopontoDisposalSerializer"]
