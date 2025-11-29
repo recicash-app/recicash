c6f9a2eefa70b001a90e433dc7df238048a396a2
Muriel <murilatorre04@gmail.com>
2025-11-29 15:44:53 -0300

    fix: lint fixes
    Signed-off-by: Muriel <murilatorre04@gmail.com>

--- frontend/apps/app-client/src/pages/BlogPage.jsx
+++ frontend/apps/app-client/src/pages/BlogPage.jsx
@@ -5,160 +5,160 @@ import FullScreenOverlay from '@shared/ui/FullScreenOverlay';
 
 function PostCard({ post }) {
 
-    const CardComponent = styled(Card)({
-        width: '300px',
-        height: '184px',
-        maxWidth: '25vw',
-        maxHeight: '25vh',
-        borderRadius: '0px',
-        cursor: 'pointer',
+  const CardComponent = styled(Card)({
+    width: '300px',
+    height: '184px',
+    maxWidth: '25vw',
+    maxHeight: '25vh',
+    borderRadius: '0px',
+    cursor: 'pointer',
         
-        // removes the ripple effect on click
-        '& .MuiTouchRipple-root': {
-            display: 'none !important', 
-        },
-    });
+    // removes the ripple effect on click
+    '& .MuiTouchRipple-root': {
+      display: 'none !important', 
+    },
+  });
 
-    const InteractiveArea = CardComponent.withComponent(CardActionArea);
+  const InteractiveArea = CardComponent.withComponent(CardActionArea);
 
-    const CardImage = styled(CardMedia)({
-        height: '65%',
-        width: '100%',
-        margin: 'auto',
-        objectFit: 'cover',
-    });
+  const CardImage = styled(CardMedia)({
+    height: '65%',
+    width: '100%',
+    margin: 'auto',
+    objectFit: 'cover',
+  });
 
-    const CardText = styled(CardContent)({
-        height: '35%',
-        display: 'flex',
-        alignItems: 'center',
-        textAlign: 'center',
-    });
+  const CardText = styled(CardContent)({
+    height: '35%',
+    display: 'flex',
+    alignItems: 'center',
+    textAlign: 'center',
+  });
 
-    const [openPost, setOpenPost] = React.useState(false);
+  const [openPost, setOpenPost] = React.useState(false);
 
-    const handleOpenPost = () => {
-        setOpenPost(true);
-    };
+  const handleOpenPost = () => {
+    setOpenPost(true);
+  };
 
-    const handleClosePost = () => {
-        setOpenPost(false);
-    };
+  const handleClosePost = () => {
+    setOpenPost(false);
+  };
 
-    const postContent = (
-        <div>
-            <Typography variant="h4" gutterBottom>
-                {post.title}
-            </Typography>
-            <Typography variant="subtitle1" gutterBottom>
+  const postContent = (
+    <div>
+      <Typography variant="h4" gutterBottom>
+        {post.title}
+      </Typography>
+      <Typography variant="subtitle1" gutterBottom>
                 Publicação: {new Date(post.created_at).toLocaleDateString()} - 
                 Ultima atualização: {new Date(post.last_edition_date).toLocaleDateString()}
-            </Typography>
+      </Typography>
             
-            <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: '10px', margin: '30px' }}>
-                {post.images.map((image, index) => (
-                    <img
-                        key={index}
-                        src={image.image_url}
-                        alt={`${post.title} - ${index + 1}`}
-                        style={{ width: '40%', height: 'auto' }}
-                    />
-                ))}
-            </div>
-            <Typography variant="body1" gutterBottom>
-                {post.text}
-            </Typography>
-        </div>
-    );
+      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: '10px', margin: '30px' }}>
+        {post.images.map((image, index) => (
+          <img
+            key={index}
+            src={image.image_url}
+            alt={`${post.title} - ${index + 1}`}
+            style={{ width: '40%', height: 'auto' }}
+          />
+        ))}
+      </div>
+      <Typography variant="body1" gutterBottom>
+        {post.text}
+      </Typography>
+    </div>
+  );
 
-    return (
-        <>
-            <CardComponent>
-                <InteractiveArea onClick={handleOpenPost}>
-                    <CardImage component="img" height="10vh" image={post.images[0].image_url} />
-                    <CardText>
-                        <Typography gutterBottom component="div">
-                            {post.title.length > 50 ? `${post.title.substring(0, 50)}...` : post.title}
-                        </Typography>
-                    </CardText>
-                </InteractiveArea>
-            </CardComponent>
-            {openPost && (
-                <FullScreenOverlay open={openPost} onClose={handleClosePost} children={postContent} />
-            )}
-        </>
-    );
+  return (
+    <>
+      <CardComponent>
+        <InteractiveArea onClick={handleOpenPost}>
+          <CardImage component="img" height="10vh" image={post.images[0].image_url} />
+          <CardText>
+            <Typography gutterBottom component="div">
+              {post.title.length > 50 ? `${post.title.substring(0, 50)}...` : post.title}
+            </Typography>
+          </CardText>
+        </InteractiveArea>
+      </CardComponent>
+      {openPost && (
+        <FullScreenOverlay open={openPost} onClose={handleClosePost} children={postContent} />
+      )}
+    </>
+  );
 }
 
 function BlogPage() {
-    const [posts, setPosts] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [searchTerm, setSearchTerm] = useState('');
-    const [filteredPosts, setFilteredPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredPosts, setFilteredPosts] = useState([]);
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-            try {
-                const response = await fetch('http://api.docker.localhost:81/api/v1/posts/');
-                const data = await response.json();
-                setPosts(data);
-                setLoading(false);
-            } catch (error) {
-                console.error('Erro ao buscar posts:', error);
-                setLoading(false);
-            }
-        };
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch('http://api.docker.localhost:81/api/v1/posts/');
+        const data = await response.json();
+        setPosts(data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Erro ao buscar posts:', error);
+        setLoading(false);
+      }
+    };
 
-        fetchPosts();
-    }, []);
+    fetchPosts();
+  }, []);
     
-    useEffect(() => {
-        const handleSearch = async () => {
-            if (searchTerm.trim() === '') {
-                setFilteredPosts([]);
-                return;
-            }
+  useEffect(() => {
+    const handleSearch = async () => {
+      if (searchTerm.trim() === '') {
+        setFilteredPosts([]);
+        return;
+      }
 
-            try {
-                const response = await fetch(`http://api.docker.localhost:81/api/v1/posts/search/?q=${searchTerm}`);
-                const data = await response.json();
-                setFilteredPosts(data.results);
-            } catch (error) {
-                console.error('Erro ao buscar posts:', error);
-            }
-        };
+      try {
+        const response = await fetch(`http://api.docker.localhost:81/api/v1/posts/search/?q=${searchTerm}`);
+        const data = await response.json();
+        setFilteredPosts(data.results);
+      } catch (error) {
+        console.error('Erro ao buscar posts:', error);
+      }
+    };
 
-        const delayDebounceFn = setTimeout(() => {
-            handleSearch();
-        }, 500); // debounce delay
+    const delayDebounceFn = setTimeout(() => {
+      handleSearch();
+    }, 500); // debounce delay
 
-        return () => clearTimeout(delayDebounceFn);
-    }, [searchTerm]);
+    return () => clearTimeout(delayDebounceFn);
+  }, [searchTerm]);
 
-    if (loading) {
-        return <Typography>Carregando posts...</Typography>;
-    }
+  if (loading) {
+    return <Typography>Carregando posts...</Typography>;
+  }
 
-    return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-            <div style={{ marginBottom: '40px', textAlign: 'center' }}>
-                <input
-                    type="text"
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                    placeholder="Buscar posts..."
-                    style={{ padding: '10px', width: '30vw', borderRadius: '5px', border: '1px solid #ccc' }}
-                />
-            </div>
-            <Grid container spacing={3} justifyContent="center">
-                {(filteredPosts.length > 0 ? filteredPosts : posts).map((post) => (
-                    <Grid item key={post.id}>
-                        <PostCard post={post} />
-                    </Grid>
-                ))}
-            </Grid>
-        </div>
-    );
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+      <div style={{ marginBottom: '40px', textAlign: 'center' }}>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Buscar posts..."
+          style={{ padding: '10px', width: '30vw', borderRadius: '5px', border: '1px solid #ccc' }}
+        />
+      </div>
+      <Grid container spacing={3} justifyContent="center">
+        {(filteredPosts.length > 0 ? filteredPosts : posts).map((post) => (
+          <Grid item key={post.id}>
+            <PostCard post={post} />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
 }
 
 export default BlogPage;
