1de70da0f79169f8ff17d573412b1825c733d241
Muriel <murilatorre04@gmail.com>
2025-11-30 01:41:26 -0300

    feat: add map style import
    Signed-off-by: Muriel <murilatorre04@gmail.com>

--- frontend/apps/app-client/src/main.jsx
+++ frontend/apps/app-client/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider, CssBaseline } from "@mui/material";
 
+import 'leaflet/dist/leaflet.css';
+
 import theme from "@shared/styles/recicashTheme.js";
 import App from './App.jsx';
 
